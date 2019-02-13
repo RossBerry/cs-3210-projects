784f46661a8332afd628a577e1979b7a47251fda
Kenneth Berry <rosskberry@gmail.com>
2019-02-13 14:24:03 -0700

    Add recursion simulated binary search function.

--- recursion_sim.py
+++ recursion_sim.py
@@ -1,18 +1,41 @@
-def binary_search(lst, n):
+"""
+recursion_sim.py
+
+Simulates a recursive function using a stack.
+"""
+
+__author__ = "Kenneth Berry"
+
+
+def binary_search_recursive(lst, n):
+    """Recursive binary search."""
     if len(lst) == 1:
-        if lst[0] == n:
-            return True
-        else:
-            return False
+        return bool(lst[0] == n)
     mid = len(lst) // 2
     if lst[mid] < n:
-        return binary_search(lst[mid:], n)
+        return binary_search_recursive(lst[mid+1:], n)
     elif lst[mid] > n:
-        return binary_search(lst[:mid], n)
+        return binary_search_recursive(lst[:mid], n)
     else:
         return True
 
-num_list = [n for n in range(10)]
+def binary_search_stack(lst, n):
+    """Binary search using a stack to simulate recursion."""
+    stack = []
+    done_stacking = False
+    is_found = False
+    while not done_stacking:
+        if len(lst) == 1:
+            stack.append(lst)
+        else:
+            mid = len(lst) // 2
 
-for n in range(20):
-    print(n, binary_search(num_list, n))
+        if len(lst) == 1:
+            return bool(lst[0] == n)
+        
+
+if __name__ == "__main__":
+    # Create list of test values
+    num_list = [n for n in range(10)]
+    for n in range(20):
+        print(n, binary_search_recursive(num_list, n))
