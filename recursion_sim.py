d8818bfb887ca3dd81ed6eba51fc518583023286
Kenneth Berry <kberry14@msudenver.edu>
2019-02-14 18:23:07 -0700

    Update stack function.

--- recursion_sim.py
+++ recursion_sim.py
@@ -13,29 +13,45 @@ def binary_search_recursive(lst, n):
         return bool(lst[0] == n)
     mid = len(lst) // 2
     if lst[mid] < n:
-        return binary_search_recursive(lst[mid+1:], n)
+        return binary_search_recursive(lst[mid + 1:], n)
     elif lst[mid] > n:
         return binary_search_recursive(lst[:mid], n)
-    else:
-        return True
+
 
 def binary_search_stack(lst, n):
     """Binary search using a stack to simulate recursion."""
+    print("looking for", n)
     stack = []
+    search_lst = lst
     done_stacking = False
     is_found = False
     while not done_stacking:
-        if len(lst) == 1:
-            stack.append(lst)
+        if len(search_lst) == 1:
+            done_stacking = True
+            is_found = bool(search_lst[0] == n)
+        mid = len(search_lst) // 2
+        if search_lst[mid] < n:
+            search_lst = search_lst[mid + 1:]
+        elif search_lst[mid] > n:
+            search_lst = search_lst[:mid]
         else:
-            mid = len(lst) // 2
+            done_stacking = True
+            is_found = True
+        stack.append(search_lst)
+    for item in stack:
+        print(item)
+        stack.pop()
+    return is_found
 
-        if len(lst) == 1:
-            return bool(lst[0] == n)
-        
 
 if __name__ == "__main__":
     # Create list of test values
     num_list = [n for n in range(10)]
+    # print("Recursive function:")
+    # for n in range(20):
+    #     print(n, binary_search_recursive(num_list, n))
+
+
+    print("Stack function:")
     for n in range(20):
-        print(n, binary_search_recursive(num_list, n))
+        print(n, binary_search_stack(num_list, n))
