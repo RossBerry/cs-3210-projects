55fd8135e49bf5254f0d5887c036cc01e4dd35e3
Kenneth Berry <rosskberry@gmail.com>
2019-03-05 09:58:53 -0700

    Add factorial.jl

--- julia/factorial.jl
+++ julia/factorial.jl
@@ -1,11 +1,12 @@
-function factorial(n)
+function my_factorial(n)
+    n = BigInt(n)
     if n == 0
         return 1
     else
-        return n * factorial(n - 1)
+        return n * my_factorial(n - 1)
     end
 end
 
-for i = 1:15
-    println(factorial(i))
+for i = 1:50
+    println(my_factorial(i))
 end
