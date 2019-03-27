c1399d4b1b862dd117aacc364c32917ef35fec0e
Kenneth Berry <kberry14@msudenver.edu>
2019-03-27 16:07:55 -0600

    Update

--- julia/Floyd.jl
+++ julia/Floyd.jl
@@ -10,14 +10,15 @@ function strip_new_line(string)
     return new_string
 end
 
-
-file_data = open("test") do file
-    read(file, String)
+function readFile(filename)
+    #table::Array{Int}(undef, size, size)
+    dataString = open(filename) do file
+        read(file, String)
+    end
+    size = parse(Int64, split(dataString)[1])
+    data = [parse(Int64, c) for c in split(dataString) if c != '\n' || c != '\r']
+    data = data[2,]
+    println(data)
 end
 
-data = strip_new_line(file_data)
-data = split(data)
-size = parse(Int64, data[1])
-println(size)
-
-#t = [size][size][2]
+readFile("test")
