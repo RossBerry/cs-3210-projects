52ed812c4eba48174ee0375a6857b56d0e6e9142
Kenneth Berry <kberry14@msudenver.edu>
2019-04-16 22:13:56 -0600

    Update to use ternary operators.

--- julia/factorial.jl
+++ julia/factorial.jl
@@ -1,19 +1,16 @@
 import Base.factorial
 
 function factorial(n::BigInt)
-    if n == 0
-        return 1
-    else
-        return n * factorial(n - 1)
-    end
+    (n == 0) ? 1 : n * factorial(n - 1)
 end
 
-for n in 0:50
-    if n < 21
-        # Use Base.factorial(n::Int)
-        println(factorial(n))
-    else
-        # Use factorial(n::BigInt)
-        println(factorial(BigInt(n)))
-    end
-end
+# for n in 0:50
+#     f = (n < 21) ? factorial(n) : factorial(BigInt(n))
+#     println(f)
+# end
+
+A = [factorial(n) if (n<21) else factorial(BigInt(n)) for n in 0:50]
+foreach(println, [factorial(n) if n<21 else factorial(BigInt(n)) for n in 0:50])
+
+l = [1, 2, 3, 4, 5]
+a = ['yes' if v == 1 else 'no' if v == 2 else 'idle' for v in l]
