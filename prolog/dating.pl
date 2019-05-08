b75b6eda1e62dbe4fc250a872769ba202ce2a0b1
Kenneth Berry <rosskberry@gmail.com>
2019-05-08 12:13:15 -0600

    Update prolog project files

--- prolog/dating.pl
+++ prolog/dating.pl
@@ -1,21 +1,105 @@
+%% dating.pl
+%% Kenneth Berry
+%% 
+%% A dating database that can be used to match people for dates.
+%% The database includes facts about 5 men and 5 women and things
+%% they like.  Matches are made based on conditions in rules for each
+%% fact/like and a perfect match is when two people match for all 
+%% the categories.
+
+
 %%---------------------- Facts ----------------------%%
 
-%%% Likes and Dislikes %%%
+% person's sex
+man(john).
+man(andy).
+man(mike).
+man(bob).
+man(bill).
+woman(anne).
+woman(jessica).
+woman(kimberly).
+woman(jill).
+woman(sophie).
 
-% likes animals
-likesanimals(john).
-likesanimals(karen).
+% person likes pets
+likespets(john).
+likespets(mike).
+likespets(anne).
+likespets(jill).
+likespets(sophie).
 
+% person likes dancing
+likesdancing(john).
+likesdancing(mike).
+likesdancing(anne).
+likesdancing(sophie).
 
-%%% Personality Traits %%%
-% outgoing
-outgoing(karen)
+% person likes books
+likesbooks(mike).
+likesbooks(bob).
+likesbooks(jessica).
+likesbooks(jill).
+likesbooks(anne).
 
-% introverted
-introverted(john)
+% person likes movies
+likesmovies(andy).
+likesmovies(mike).
+likesmovies(bob).
+likesmovies(bill).
+likesmovies(anne).
+likesmovies(kimberly).
+likesmovies(sophie).
+
+% person enjoys outdoors
+likesoutdoors(john).
+likesoutdoors(andy).
+likesoutdoors(bill).
+likesoutdoors(kimberly).
+likesoutdoors(sophie).
 
 
 %%---------------------- Rules ----------------------%%
 
-%%------------------- Procedures --------------------%%
-compatible(X, Y) :-  
+% match if people are different sexes
+matchsex(X, Y) :- ((man(X) , woman(Y)) |
+                  (woman(X), man(Y))),
+                  (X \= Y).
+
+% match if both people like or dislike pets
+matchpets(X, Y) :- ((likespets(X) , likespets(Y)) |
+                   (not(likespets(X)) , not(likespets(Y)))),
+                   matchsex(X, Y),
+                   (X \= Y).
+
+% match if both people like or dislike dancing
+matchdancing(X, Y) :- ((likesdancing(X) , likesdancing(Y)) |
+                      (not(likesdancing(X)) , not(likesdancing(Y)))),
+                      matchsex(X, Y),
+                      (X \= Y).
+
+% match if both people like or dislike books
+matchbooks(X, Y) :- ((likesbooks(X) , likesbooks(Y)) |
+                    (not(likesbooks(X)) , not(likesbooks(Y)))),
+                    matchsex(X, Y),
+                    (X \= Y).
+
+% match if both people like or dislike movies
+matchmovies(X, Y) :- ((likesmovies(X) , likesmovies(Y)) |
+                     (not(likesmovies(X)) , not(likesmovies(Y)))),
+                     matchsex(X, Y),
+                     (X \= Y).
+
+% match if both people like or dislike the outdoors
+matchoutdoors(X, Y) :- ((likesoutdoors(X) , likesoutdoors(Y)) |
+                       (not(likesoutdoors(X)) , not(likesoutdoors(Y)))),
+                       matchsex(X, Y),
+                       (X \= Y).
+
+% perfect match if both people like or dislike all the same things
+perfectmatch(X, Y) :-  matchsex(X, Y),
+                    matchpets(X, Y),
+                    matchdancing(X, Y),
+                    matchbooks(X, Y),
+                    matchmovies(X, Y),
+                    matchoutdoors(X, Y).
--- prolog/dietary.pl
+++ prolog/dietary.pl
@@ -1,10 +1,16 @@
+%% dietary.pl
+%% Kenneth Berry
+%% 
+%% A database that has facts and rules for food and meals.
+
+
 %%---------------------- Food Facts & Rules ----------------------%%
 
 % vegatables
 vegatable(carrot).
 vegatable(corn).
-vegatable(rice).
 vegatable(beans).
+vegatable(cucumber).
 
 % fruits
 fruit(apple).
@@ -23,18 +29,27 @@ snack(chips).
 snack(popcorn).
 snack(fries).
 snack(candy).
-snack(A) :- fruit(A).
-snack(A) :- dessert(A).
+snack(Food) :- fruit(Food).   % all fruits are snacks
+snack(Food) :- dessert(Food). % all desserts are snacks
 
 % proteins
 protein(steak).
 protein(fish).
 protein(chicken).
-protein(beans).
+protein(pork).
+protein(bacon).
 
 % starches
 starch(corn).
 starch(potato).
+starch(bread).
+starch(rice).
+
+% breakfast food
+breakfastfood(pancakes).
+breakfastfood(waffles).
+breakfastfood(bacon).
+breakfastfood(Food) :- fruit(Food).  % fruits are breakfast food
 
 % breakfast drinks
 breakfastdrink(juice).
@@ -42,21 +57,30 @@ breakfastdrink(coffee).
 breakfastdrink(milk).
 
 % drinks
-drink(_) :- breakfastdrink(_).
+drink(Drink) :- breakfastdrink(Drink). % breakfast drinks are drinks
 drink(water).
 drink(tea).
 drink(coffee).
 drink(soda).
 
+
 %%---------------------- Meal Rules ----------------------%%
 
+% breakfast can consist of a breakfastfood and a breakfastdrink
+breakfast(A, B) :- ((breakfastfood(A), breakfastdrink(B)) |
+                   (breakfastdrink(A), breakfastfood(B))),
+                   (A \= B).
 
+% A meal can consist of a snack and a drink
 meal(A, B) :- (snack(A) | snack(B)),
               (drink(A) | drink(B)),
               (A \= B).
 
+% A meal can consist of a protein, veggie, dessert, and drink
 meal(A, B, C, D) :- protein(A), vegatable(B), dessert(C), drink(D).
 
+% A meal can consist of a protein, veggie, startch, and drink
 meal(A, B, C, D) :- protein(A), vegatable(B), starch(C), drink(D).
-meal(A, B, C, D, E) :- protein(A), vegatable(B), starch(C), dessert(D), drink(E).
 
+% A meal can consist of a protein, veggie, starch, dessert, and drink
+meal(A, B, C, D, E) :- protein(A), vegatable(B), starch(C), dessert(D), drink(E).
